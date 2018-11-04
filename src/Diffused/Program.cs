be9c0c70976a13a4403a7071c5a1ca3bdfa98f43
Daniel Grimes <dan@dcgnet.co.uk>
2018-11-04 13:10:27 +0000

    Add projects

---
+++ src/Diffused/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Diffused
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
